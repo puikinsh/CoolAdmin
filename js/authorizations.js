612c0f8c8853f103aa53433f1240592c967f402f
onaxel <axel.s09kpo@gmail.com>
2024-05-21 08:26:49 -0300

    feat: add gmail authorization

--- js/authorizations.js
+++ js/authorizations.js
@@ -1,22 +1,42 @@
 import axios from "axios";
-import { URL_MS_GOOGLE } from "../secrets";
-import { getSessionTokensWithRefresh } from "./authentication";
+import { URL_MS_GOOGLE, X_API_KEY } from "../secrets";
+import { getUserInfo, refreshAndGetTokens } from "./authentication";
 
-(async function($){
+(async function($) {
     try {
-        document.getElementById('gmail-auth').addEventListener('submit', async (e) => {
-            e.preventDefault();
-            console.log("hola")
+        document
+            .getElementById("gmail-auth")
+            .addEventListener("submit", async (e) => {
+                e.preventDefault();
 
-            await refreshAndGetTokens()
-            // const {data} = await axios.get({url:`${URL_MS_GOOGLE}/google-auth-webhook`, headers:{
-            //     accessT: localStorage.getItem('AccessToken'),
-            //     refreshT: localStorage.getItem('RefreshToken'),
-            //     idT: localStorage.getItem('IdToken'),
-            // }})
-            // console.log(data)
-        })
+                await refreshAndGetTokens();
+                const { data } = await axios.get(
+                    `${URL_MS_GOOGLE}/google-auth-webhook`,
+                    {
+                        headers: {
+                            "x-api-key": X_API_KEY,
+                        },
+                    }
+                );
+
+                if (data.url) {
+                    window.open(data.url, "_blank"); // Redirige al usuario a la URL de Gmail
+                } else {
+                    console.error(
+                        "URL de Gmail no encontrada en la respuesta",
+                        data
+                    );
+                }
+            });
+        const button = document.getElementById("gmailButton");
+
+        if (button) {
+            const info = await getUserInfo();
+            if (info.gmailAuthorization) {
+                button.innerHTML = "GMAIL AUTHORIZED";
+            }
+        }
     } catch (error) {
         console.log(error);
     }
-})()
+})();
