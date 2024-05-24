36fa697bc02f6cf1281b858c8e49f05778b4156c
onaxel <axel.s09kpo@gmail.com>
2024-05-24 08:28:16 -0300

    feat: add redirect if user is already signed in

--- js/auth/loginLogout.js
+++ js/auth/loginLogout.js
@@ -4,38 +4,43 @@ const { logout, login, getUserInfo } = require("../authentication");
     document.addEventListener("DOMContentLoaded", async () => {
         const logoutButton = document.getElementById("logout-button");
         const loginForm = document.getElementById("login-form");
-        // try {
-        //     const userData = await getUserInfo();
-        //     console.log(window.location);
-        //     // window.location.href = "/index.html";
-        // } catch (error) {
-        if (loginForm) {
-            loginForm.addEventListener("submit", async (e) => {
-                e.preventDefault();
+        try {
+            const userData = await getUserInfo();
+            if (window.location.pathname === "/login.html" && userData) {
+                window.location.href = "/index.html";
+            }
+        } catch (error) {
+            if (loginForm) {
+                loginForm.addEventListener("submit", async (e) => {
+                    e.preventDefault();
 
-                const username = document.getElementById("username").value;
-                const password = document.getElementById("password").value;
+                    const username = document.getElementById("username").value;
+                    const password = document.getElementById("password").value;
+                    try {
+                        const data = await login({ username, password });
+                        if (data === "Successfully logged in") {
+                            window.location.href = "/index.html";
+                        }
+                    } catch (error) {
+                        window.alert(error.message);
+                    }
+                });
+            }
+            // }
 
-                const data = await login({ username, password });
-                if (data === "Successfully logged in") {
-                    window.location.href = "/index.html";
-                }
-            });
-        }
-        // }
+            if (logoutButton) {
+                logoutButton.addEventListener("click", async (e) => {
+                    e.preventDefault();
+                    console.log(e);
 
-        if (logoutButton) {
-            logoutButton.addEventListener("click", async (e) => {
-                e.preventDefault();
-                console.log(e);
-
-                try {
-                    await logout();
-                    window.location.href = "/login.html";
-                } catch (error) {
-                    console.error("Error during logout:", error);
-                }
-            });
+                    try {
+                        await logout();
+                        window.location.href = "/login.html";
+                    } catch (error) {
+                        console.error("Error during logout:", error);
+                    }
+                });
+            }
         }
     });
 })();
